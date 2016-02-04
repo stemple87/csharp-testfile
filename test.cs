b3c8f01bea28814e378e63713277295bf935bd41
Simon Temple and Yvonna Contreras <simonctemple@live.com>
2016-02-03 19:20:40 -0800

    continued with treehouse project

--- test.cs
+++ test.cs
@@ -6,31 +6,62 @@ namespace Treehouse
     {
         static void Main()
         {
-            int runningTotal = 0;
+            double runningTotal = 0;
             bool keepGoing = true;
 
             while (keepGoing)
             {
 
-
+                
                 // Prompt the user for minutes exercised
                 System.Console.Write("Enter how many minutes you exercised or type \"quit\" to exit: ");
                 string entry = Console.ReadLine();
 
-                if (entry == "quit")
+                if (entry.ToLower() == "quit")
                 {
                     keepGoing = false;
                 }
                 else
                 {
-                    int minutes = int.Parse(entry);
+                    try
+                    {
+                        double minutes = double.Parse(entry);
+
+                        if(minutes <= 0)
+                        {
+                            Console.WriteLine(minutes + " is not an acceptable value.");
+                            continue;
+                        }
+                        else if(minutes <= 10)
+                        {
+                            Console.WriteLine("Better than nothing, am I right?");
+                        }
+                        else if(minutes <= 30)
+                        {
+                            Console.WriteLine("YWay to go hot stuff!");
+                        }
+                        else if(minutes <= 60)
+                        {
+                            Console.WriteLine("You must be a ninja warrior in training!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Okay, now you're just showing off!");
+                        }
 
 
-                    // Add minutes exercised to total
-                    runningTotal = runningTotal + minutes;
+                        // Add minutes exercised to total
+                        runningTotal = runningTotal + minutes;
 
-                    // Display total minutes exercised to the screen
-                    Console.WriteLine("You've exercised for " + entry + " minutes");
+                        // Display total minutes exercised to the screen
+                        Console.WriteLine("You've exercised for " + runningTotal + " minutes");
+                    }
+                    catch(FormatException)
+                    {
+                        Console.WriteLine("That is not valid input.");
+                        continue;
+                    }
+
                 }
                 // Repeat until the user quits
             }
